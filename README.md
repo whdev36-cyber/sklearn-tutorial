8df31376e4c2251e2ce3b74fc16f40dd4416d3f6
WCyber <whdev36@gmail.com>
2025-05-18 12:34:29 +0500

    fix: correct filename in data loading for example code

--- README.md
+++
@@ -1,7 +0,0 @@
-# Scikit-Learn Tutorial
-
-
-## Examples
-- [Decision Trees](./examples/code00.py)
-- [Decision Trees with Plots](./examples/code01.py)
-- [KNN Classifier](./examples/code03.py)
--- examples/code00.py
+++ examples/code00.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('data/sklearn_test_data.csv.csv', encoding='utf-8')
+df = pd.read_csv('data/sklearn_test_data.csv', encoding='utf-8')
 
 X = df.drop(columns=['education'])
 y = df['income']
