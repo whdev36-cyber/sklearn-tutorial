beaa31cdff63af98451a95ee6cf385b5f4dead72
WCyber <whdev36@gmail.com>
2025-05-18 12:32:55 +0500

    feat: enhance data processing in example by adding train-test split and descriptive statistics

--- examples/code00.py
+++ examples/code00.py
@@ -4,5 +4,20 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('data/sklearn_test_data.csv.csv', encoding='utf-8')
 
 X = df.drop(columns=['education'])
+y = df['income']
 
-print(df)
+# print(X.head())
+# print(X.shape)
+# print(y.shape)
+# print(y.head())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=11, test_size=0.2)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+print(X_train.head())
+print(X_train.describe())
+print(X_train.describe().round(3))
